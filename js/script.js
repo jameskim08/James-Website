dea29f5757d9eee14f88d256b24bc2dbddb6f4dd
James Kim <jamesyk1997@gmail.com>
2016-05-04 00:08:52 -0400

    Added some javascript. There are some bugs though..

--- js/script.js
+++ js/script.js
@@ -13,3 +13,77 @@
 //         }
 //     }
 // });
+
+var $aboutDetails = $("#about-details");
+var $experienceDetails = $("#experience-details");
+var $projectsCarousel = $("#projects-carousel");
+var numProjects = 2;
+
+
+function removeCollapse(htmlID) {
+    htmlID.addClass('in');
+}
+
+function addCollapse(htmlID) {
+    htmlID.removeClass('in');
+}
+
+function alignProjectDetails(){
+    for (var i= 1; i<=numProjects; i++){
+        var $project = $(".project-" + i )
+        var $projectDetails = $("#project-" + i + "-detail");
+
+        if ($project.hasClass("active")){
+            addCollapse($projectDetails);
+        } else {
+            removeCollapse($projectDetails);
+        }
+    }
+}
+
+//------------------------------------------//
+
+// Code on startup
+
+$(document).ready(function() {
+    if ($(window).width() > 800){
+        removeCollapse($aboutDetails);
+        removeCollapse($experienceDetails);
+        removeCollapse($projectsCarousel);
+    }
+})
+
+//------------------------------------------//
+
+// Removing Collapse when going on Desktop Version
+
+$(window).resize(function() {
+    if ($(window).width() > 800){
+        removeCollapse($aboutDetails);
+        removeCollapse($experienceDetails);
+        removeCollapse($projectsCarousel);
+    }
+});
+
+//-----------------------------------------//
+
+// Projects Detail are aligned with its respective Project
+
+$(document).on("click", "#projects-carousel .carousel-control", function(){
+    alignProjectDetails();
+});
+
+$(document).on("click", "#projects-heading .plus-button", function(){
+    if ($projectsCarousel.attr("display") == "none"){
+        for (var i= 1; i<=numProjects; i++){
+            //code to add class
+        } 
+    } else {
+        for (var i= 1; i<=numProjects; i++){
+            var $projectDetails = $("#project-" + i + "-detail");
+            addCollapse($projectDetails);
+        } 
+    }
+})
+
+//-----------------------------------------//
